05563b28ff661696fc2c107f9885694352e8382d
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-09-20 14:49:21 +1000

    No need for Letterboxes

--- scriptslibrary/Sections/ForegroundSection.cs
+++ scriptslibrary/Sections/ForegroundSection.cs
@@ -10,26 +10,26 @@ public class ForegroundSection : Section
         var manager = GetManager<ForegroundManager>();
         manager.generateSprites();
         // Intro //
-        manager.LetterboxFade(17890, 1);
-        manager.LetterboxFade(28074, 0);
-        manager.LetterboxScale(OsbEasing.InOutCubic, 17890, 18206, 0, 80);
-        manager.LetterboxScale(OsbEasing.InOutCubic, 26811, 28074, 80, 240);
-        manager.LetterboxColor(17890,Color4.Black);
+        // manager.LetterboxFade(17890, 1);
+        // manager.LetterboxFade(28074, 0);
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 17890, 18206, 0, 80);
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 26811, 28074, 80, 240);
+        // manager.LetterboxColor(17890,Color4.Black);
 
         
 
-        // After Credits //
-        manager.LetterboxScale(OsbEasing.InOutCubic, 139153, 139469, 0, 80);
-        manager.LetterboxFade(139153, 1);
-        manager.LetterboxFade(146811, 0);
-        manager.LetterboxScale(OsbEasing.InOutCubic, 144048, 144284, 80, 150);
-        manager.LetterboxScale(OsbEasing.InOutElastic, 146416, 146811, 150, 0);
+        // // After Credits //
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 139153, 139469, 0, 80);
+        // manager.LetterboxFade(139153, 1);
+        // manager.LetterboxFade(146811, 0);
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 144048, 144284, 80, 150);
+        // manager.LetterboxScale(OsbEasing.InOutElastic, 146416, 146811, 150, 0);
 
-        // Ending
-        manager.LetterboxScale(OsbEasing.InOutCubic, 305811, 305969, 0, 80);
-        manager.LetterboxFade(305811, 1);
-        manager.LetterboxFade(310706, 0);
-        manager.LetterboxScale(OsbEasing.InOutCubic, 310548, 310706, 80, 240);
+        // // Ending
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 305811, 305969, 0, 80);
+        // manager.LetterboxFade(305811, 1);
+        // manager.LetterboxFade(310706, 0);
+        // manager.LetterboxScale(OsbEasing.InOutCubic, 310548, 310706, 80, 240);
         
         // Vignettes
         manager.vigenetteFade(284, 0.8);

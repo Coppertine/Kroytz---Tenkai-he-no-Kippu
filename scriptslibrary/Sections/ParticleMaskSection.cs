9efc328d9fa5d20f248060f3f79b77ae324550b1
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-09-29 21:07:54 +1000

    Completed the masks at least

--- scriptslibrary/Sections/ParticleMaskSection.cs
+++ scriptslibrary/Sections/ParticleMaskSection.cs
@@ -16,10 +16,25 @@ public class ParticleMaskSection : Section
     
         mask.Fade(47969, 0);
         mask.Color(47969, Color4.White);
+        // Pre-kiai 1
         mask.Fade(68495, 1);
         mask.Fade(83258, 83811, 1,0);
+        // Kiai 1
         mask.Fade(88706, 1);
         mask.Fade(129127,0);
+
+        // Pre-Kiai 2
+        mask.Fade(149337, 1);
+        mask.Fade(164416, 164653, 1,0);
+        //Kiai 2
+        mask.Fade(169548, 1);
+        mask.Fade(209258, 209969, 1, 0);
+
+        // After Solo
+        mask.Fade(230179, 1);
+        mask.Fade(247785, 247942, 1, 0);
+        mask.Fade(250390, 1);
+        mask.Fade(290732, 290890, 1, 0);
 
     }
 }
